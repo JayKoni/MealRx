246e22d223721b72d48b024f32c44ae39834e573
Jay K <jaykoni@yahoo.com>
2025-08-13 13:56:32 -0700

    Deploy to GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "8f3175778db702184b99daa9188253c8
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "cff2f3b6e31e411a730635e51aaa9a98",
+"flutter_bootstrap.js": "d3cbc29bd746b0707dfb0d69e5eacefa",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
