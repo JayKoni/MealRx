ffa136c0e0e3c5ba1f03979b4ffda725bbd17a53
Jay K <jaykoni@yahoo.com>
2025-08-18 12:07:33 -0700

    Deploy web release

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3632948476"
+    serviceWorkerVersion: "122782251"
   }
 });
