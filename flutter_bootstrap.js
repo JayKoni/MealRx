9d710d312e7ce32f4a65d3d698da4751936028eb
Jay K <jaykoni@yahoo.com>
2025-08-18 22:46:31 -0700

    Deploy web release with cache buster

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "850421951"
+    serviceWorkerVersion: "1288815775"
   }
 });
